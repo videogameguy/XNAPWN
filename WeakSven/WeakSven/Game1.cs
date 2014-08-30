66041ae738c16d15131bfbafc89fcde1baab8b7a
Brent Farris <brent@farrisarts.com>
2014-08-30 13:28:17 -0700

    Initial content commit

---
+++ WeakSven/WeakSven/Game1.cs
@@ -0,0 +1,55 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WeakSven
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+			// Comment the following if you don't want to see the mouse
+			IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent() { }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+			if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+				this.Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+			spriteBatch.Begin();
+
+
+
+			spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
